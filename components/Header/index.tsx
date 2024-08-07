764cca0a8e1c0360d3a3fc9942abc5e8efa2609e
Scott Berenzweig <scott@betterbrand.com>
2024-08-06 17:41:11 -0700

    chore: Remove commented out code for bidform in Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -47,9 +47,9 @@ const Header = () => {
         <Link href="/staking">Weights Staking</Link>
       </Menu.Item>
 
-{/* <Menu.Item key="bidform">
+    <Menu.Item key="bidform">
         <Link href="/bidform">Bid Form</Link>
-      </Menu.Item> */}
+      </Menu.Item>
 
       <Menu.Item key="profile">
         <Link href="/profile">Profile</Link>
@@ -90,10 +90,9 @@ const Header = () => {
                 <Link href="/staking" className="">
                   Weights Staking
                 </Link>
-
-                <Link href="/bidform" className="">
+                {/* <Link href="/bidform" className="">
                   Bid Form
-                </Link>
+                </Link> */}
 
                 <Link href="/profile" className="">
                   Profile
