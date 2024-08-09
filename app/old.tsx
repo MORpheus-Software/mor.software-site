401804da4920245c6a755898d73a4018b1583c02
sab4a <saba.meskhi.1@btu.edu.ge>
2024-08-09 16:21:20 +0400

    build fix

--- app/old.tsx
+++ app/old.tsx
@@ -1,4 +1,4 @@
-import { Main } from "@/components/Main";
+// import { Main } from "@/components/Main";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -9,9 +9,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow  sm:p-8">
-        <Main />
-      </main>
+      <main className="flex-grow  sm:p-8">{/* <Main /> */}</main>
       <Footer />
     </div>
   );
